b9ce70d501bbe708a400a141e6adaeda4786c4e7
Pierre <pierreminier64@gmail.com>
2024-11-30 12:50:33 +0100

    lien HTTPS + test pop up description

--- script/load_header-footer.js
+++ script/load_header-footer.js
@@ -9,5 +9,5 @@ async function load(file, elementId) {
 }
 
 // Appel de la fonction pour charger le header et le footer
-load('includes/header.html', 'header');
-load('includes/footer.html', 'footer');
+load('https://jed-spi.github.io/includes/header.html', 'header');
+load('https://jed-spi.github.io/includes/footer.html', 'footer');
